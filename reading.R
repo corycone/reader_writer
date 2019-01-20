e6c8b86e1066693fbb12f3989c0cd615a307e11a
Cory Cone <incognitocory@gmail.com>
2019-01-19 19:37:55 -0500

    minor edits

--- reading.R
+++ reading.R
@@ -5,4 +5,10 @@ reader <- read.xlsx("data/reader_data.xlsx", sheetName = "reading")
 
 ggplot(reader, aes(x = year, y = hours, fill = reader$month)) +
   geom_bar(stat = "identity") +
-  theme_light()
+  scale_color_brewer(palette = "Set2") +
+  theme_light() +
+  labs(title = "5 Years of Reading by K. Edwin Fritz", 
+       x = "Year", 
+       y = "Hours Read",
+       caption = "The personal logs of author K. Edwin Fritz")
+  
