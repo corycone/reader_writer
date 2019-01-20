bbb581ab0d06fb0c6e0de5aa3f74206a65204bd3
Cory Cone <incognitocory@gmail.com>
2019-01-20 14:37:44 -0500

    draft of heatmap of reading

--- 2015r.R
+++ 2015r.R
@@ -1,17 +1,15 @@
 library(xlsx)
 library(ggplot2)
 
-r2015 <- read.xlsx("data/reader_data.xlsx", sheetName = "2015ra")
+r2015 <- read.xlsx("data/all_reading.xlsx", sheetName = "all_reading")
 
-ggplot(r2015, aes(x = Month, y = Minutes)) +
-  geom_bar(stat = "identity") +
+ggplot(r2015, aes(x = reorder(day, day), Month, size = minutes, fill = minutes)) +
+  geom_raster() +
+  scale_fill_gradient(low = "red", high = "yellow") +
   theme_light() +
-  labs(title = "2015 Reading | K. Edwin Fritz", 
-       x = "Month", 
-       y = "Minutes Read",
-       caption = "The personal logs of author K. Edwin Fritz")
-
-str(reader)
-
-
+  labs(title = "Heatmap of Reading Minutes | K. Edwin Fritz", 
+       x = "Day", 
+       y = "Month",
+       caption = "The personal logs of author K. Edwin Fritz") +
+  coord_equal()
 
