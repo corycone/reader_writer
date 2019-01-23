302672932f19ac4af3a7dc7b59f0fdaf4eb83c54
Cory Cone <incognitocory@gmail.com>
2019-01-22 22:00:24 -0500

    fixed month order

--- reading_area_facet.R
+++ reading_area_facet.R
@@ -4,12 +4,17 @@ library(ggplot2)
 r_comp <- read.xlsx("data/all_reading.xlsx", sheetName = "all_reading")
 r_comp_15 <- subset(r_comp, Year == "2015")
 r_comp_15[is.na(r_comp_15)] <- 0
+r_comp_15$facet = factor(r_comp_15$Month, 
+                         levels = c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))
+
+
+
 
 
 ggplot(r_comp_15, aes(x=day, y = minutes/60, fill = Month)) +
   scale_fill_brewer(palette = "Set3") +
   geom_area() +
-  facet_wrap(~Month, strip.position = "bottom") +
+  facet_wrap(~facet, strip.position = "bottom") +
   labs(title = "Time Spent Reading in 2015", 
        x = "Day of the Month", 
        y = "Hours",
