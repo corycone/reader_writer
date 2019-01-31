9fc5f8b5cf5796f2562247c99bda98190bee1948
Cory Cone <incognitocory@gmail.com>
2019-01-30 21:33:59 -0500

    initial animations

--- animate.R
+++ animate.R
@@ -10,7 +10,22 @@ anim <- ggplot(july_2018, aes(x=day, y = minutes)) +
        y = "Hours") +
   scale_x_continuous(breaks = july_2018$day) +
   g_theme_example +
-  transition_reveal(day, keep_last = TRUE)
+  transition_reveal(minutes, keep_last = TRUE)
   
 animate(anim, fps = 30, duration = 2)  
+
+
+anim2 <- ggplot(r_years$`2018`, aes(x=day, y = minutes/60)) +
+  geom_area(fill = fill_color) +
+  scale_y_reverse() +
+  facet_wrap(~facet, strip.position = "top") +
+  labs(title = "2018", 
+       x = "Day of the Month", 
+       y = "Hours",
+       caption = "Source: K. Edwin Fritz, www.fritzfiction.com
+       Graphic: Cory Cone, dataviz.corycone.com") +
+  g_theme + coord_cartesian(ylim=c(0, 3)) +
+  transition_reveal(minutes, keep_last = TRUE)
+
+animate(anim2, fps = 30, duration = 4)  
 
